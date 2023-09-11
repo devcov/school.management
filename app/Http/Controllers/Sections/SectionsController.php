4b9a62bc2117ff1abf920055d03ea8b9d17602b4
mohamedhann <devcov2387@gmail.com>
2023-09-12 01:09:28 +0200

    new update

--- app/Http/Controllers/Sections/SectionsController.php
+++ app/Http/Controllers/Sections/SectionsController.php
@@ -100,40 +100,40 @@ class SectionsController extends Controller
     {
         //
 
-        // try {
-        //     $validated = $request->validated();
-        //     $Sections = Section::findOrFail($request->id);
+        try {
+            $validated = $request->validated();
+            $Sections = Section::findOrFail($request->id);
 
-        //     $Sections->Name_Section = ['ar' => $request->Name_Section_Ar, 'en' => $request->Name_Section_En];
-        //     $Sections->Grade_id = $request->Grade_id;
-        //     $Sections->Class_id = $request->Class_id;
+            $Sections->Name_Section = ['ar' => $request->Name_Section_Ar, 'en' => $request->Name_Section_En];
+            $Sections->Grade_id = $request->Grade_id;
+            $Sections->Class_id = $request->Class_id;
 
-        //     if(isset($request->Status)) {
-        //       $Sections->Status = 1;
-        //     } else {
-        //       $Sections->Status = 2;
-        //     }
-
-
-             // update pivot tABLE
-            //   if (isset($request->teacher_id)) {
-            //       $Sections->teachers()->sync($request->teacher_id);
-            //   } else {
-            //       $Sections->teachers()->sync(array());
-            //   }
+            if(isset($request->Status)) {
+              $Sections->Status = 1;
+            } else {
+              $Sections->Status = 2;
+            }
 
 
-        //     $Sections->save();
-        //     toastr()->success(trans('messages.Update'));
+           //  update pivot tABLE
+              if (isset($request->teacher_id)) {
+                  $Sections->teachers()->sync($request->teacher_id);
+              } else {
+                  $Sections->teachers()->sync(array());
+              }
 
-        //     return redirect()->route('Sections.index');
-        // }
-        // catch
-        // (\Exception $e) {
-        //     return redirect()->back()->withErrors(['error' => $e->getMessage()]);
-        // }
 
-        return $request;
+            $Sections->save();
+            toastr()->success(trans('messages.Update'));
+
+            return redirect()->route('Sections.index');
+        }
+        catch
+        (\Exception $e) {
+            return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+        }
+
+      
     }
 
     /**
--- database/migrations/2023_08_24_165211_create_foreign_keys.php
+++ database/migrations/2023_08_24_165211_create_foreign_keys.php
@@ -16,17 +16,22 @@ class CreateForeignKeys extends Migration {
 		});
 
         Schema::table('sections', function(Blueprint $table) {
-            $table->foreign('Grade_id')->references('id')->on('Grades')
-                ->onDelete('cascade');
+            $table->foreign('Grade_id')->references('id')->on('Grads')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
         });
 
         Schema::table('my__parents', function(Blueprint $table) {
             $table->foreign('Nationality_Father_id')->references('id')->on('nationalities');
-            $table->foreign('Blood_Type_Father_id')->references('id')->on('type__bloods');
+            $table->foreign('Blood_Type_Father_id')->references('id')->on('type_bloods');
             $table->foreign('Religion_Father_id')->references('id')->on('religions');
             $table->foreign('Nationality_Mother_id')->references('id')->on('nationalities');
-            $table->foreign('Blood_Type_Mother_id')->references('id')->on('type__bloods');
+            $table->foreign('Blood_Type_Mother_id')->references('id')->on('type_bloods');
             $table->foreign('Religion_Mother_id')->references('id')->on('religions');
+        });
+
+        Schema::table('parent_attachments', function(Blueprint $table) {
+            $table->foreign('parent_id')->references('id')->on('my__parents');
         });
 	}
 
